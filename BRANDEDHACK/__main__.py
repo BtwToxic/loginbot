41013f6c6597d02481237f41abfc0cd0eb60b0f1
BtwToxic <btwalexxd@gmail.com>
2026-01-02 18:51:11 +0530

    Update __main__.py

--- BRANDEDHACK/__main__.py
+++ BRANDEDHACK/__main__.py
@@ -9,7 +9,7 @@ from BRANDEDHACK.modules import ALL_MODULES
 async def start_bot():
     for all_module in ALL_MODULES:
         importlib.import_module("BRANDEDHACK.modules." + all_module)
-    LOG.print("[bold yellow]𝗛𝗔𝗖𝗞 𝗕𝗜𝗧 𝗦𝗧𝗔𝗥𝗧𝗘𝗗 𝗡𝗢𝗪 𝗙𝗨𝗖𝗞 𝗔𝗟𝗟 𝗧𝗚 𝗜𝗗")
+    LOG.print("[bold yellow]𝗛𝗔𝗖𝗞 𝗕O𝗧 𝗦𝗧𝗔𝗥𝗧𝗘𝗗 𝗡𝗢𝗪 𝗙𝗨𝗖𝗞 𝗔𝗟𝗟 𝗧𝗚 𝗜𝗗")
     await idle() 
     LOG.print("[bold red]𝗖𝗔𝗡𝗖𝗟𝗘 𝗔𝗟𝗟 𝗧𝗔𝗦𝗞🤐..........")
 
